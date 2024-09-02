ea7e0ca5b51a3ddab6f856abea2fc56dd9287e5f
rom-vtn <romahvito@gmail.com>
2024-09-02 22:19:06 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/rom-vtn/go-nexttransit
+
+go 1.22.4
+
+require github.com/artonge/go-gtfs v1.3.4
+
+require github.com/artonge/go-csv-tag/v2 v2.2.0 //indirect
---
+++ parse_feed.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"time"
+
+	gtfs "github.com/artonge/go-gtfs"
+)
+
+const DIR_PATH = "./lemet/"
+const WANTED_LAT = 49.10123225574134
+const WANTED_LON = 6.2228643894195566
+
+const KM_TO_DEGREES = 0.009
+
+const DIST_THRESHOLD_KM = 0.3
+
+func main() {
+	sights, err := GetNextBuses(WANTED_LAT, WANTED_LON, DIR_PATH)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, sight := range sights {
+		fmt.Printf("Bus: %s -> %s, at %s\n", sight.RouteName, sight.Headsign, sight.Timestamp.Format("15:04"))
+	}
+}
+
+type Sighting struct {
+	Timestamp time.Time
+	RouteName string
+	Headsign  string
+}
+
+func GetNextBuses(lat, lon float64, dirPath string) ([]Sighting, error) {
+	feed, err := gtfs.Load(dirPath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	closeStopsMap := getCloseStops(feed, lat, lon)
+
+	allTripsMap := make(map[string]gtfs.Trip)
+	for _, trip := range feed.Trips {
+		allTripsMap[trip.ID] = trip
+	}
+	allRoutesMap := make(map[string]gtfs.Route)
+	for _, route := range feed.Routes {
+		allRoutesMap[route.ID] = route
+	}
+
+	today := time.Now().Truncate(24 * time.Hour)
+	activeServicesMap := getActiveServicesOn(feed, today)
+
+	var sights []Sighting
+	for _, stopTime := range feed.StopsTimes {
+		_, ok := closeStopsMap[stopTime.StopID]
+		if !ok {
+			continue
+		}
+		trip, ok := allTripsMap[stopTime.TripID]
+		if !ok {
+			panic("trip not part of all trips map")
+		}
+		if !activeServicesMap[trip.ServiceID] {
+			continue
+		}
+		//do the actual sight
+		departureOffset := timeDurationFromGtfsString(stopTime.Departure)
+		timestamp := today.Add(departureOffset)
+		routeName := allRoutesMap[trip.RouteID].ShortName
+		headsign := trip.Headsign
+		sight := Sighting{
+			Timestamp: timestamp,
+			RouteName: routeName,
+			Headsign:  headsign,
+		}
+		sights = append(sights, sight)
+	}
+
+	sort.Slice(sights, func(i, j int) bool {
+		return sights[i].Timestamp.Before(sights[j].Timestamp)
+	})
+
+	return sights, nil
+}
+
+func getCloseStops(feed *gtfs.GTFS, wantedLat, wantedLon float64) map[string]gtfs.Stop {
+	stops := make(map[string]gtfs.Stop)
+	for _, stop := range feed.Stops {
+		latDiff := (stop.Latitude - wantedLat) / KM_TO_DEGREES
+		lonDiff := (stop.Longitude - wantedLon) / KM_TO_DEGREES
+		distSquared := latDiff*latDiff + lonDiff*lonDiff
+		if distSquared < DIST_THRESHOLD_KM*DIST_THRESHOLD_KM {
+			stops[stop.ID] = stop
+		}
+	}
+	return stops
+}
+
+// converts YYYYMMDD to time.Time
+func dayFromFromGtfsString(gtfsDayString string) time.Time {
+	time, err := time.Parse("20060102", gtfsDayString)
+	if err != nil {
+		panic(err)
+	}
+	return time
+}
+
+// converts hh:mm:ss to time.Duration
+// note: gtfs times may go above 24:00:00 hence the need to do our own parsing
+func timeDurationFromGtfsString(gtfsTimeString string) time.Duration {
+	hourString := gtfsTimeString[0:2]
+	minuteString := gtfsTimeString[3:5]
+	secondString := gtfsTimeString[6:8]
+
+	hour, err := strconv.ParseInt(hourString, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	minute, err := strconv.ParseInt(minuteString, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	second, err := strconv.ParseInt(secondString, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return time.Duration(hour)*time.Hour + time.Duration(minute)*time.Minute + time.Duration(second)*time.Second
+}
+
+func isCalendarActiveOn(calendar gtfs.Calendar, weekday time.Weekday) bool {
+	isDayActive := 0
+	switch weekday {
+	case time.Monday:
+		isDayActive = calendar.Monday
+	case time.Tuesday:
+		isDayActive = calendar.Tuesday
+	case time.Wednesday:
+		isDayActive = calendar.Wednesday
+	case time.Thursday:
+		isDayActive = calendar.Thursday
+	case time.Friday:
+		isDayActive = calendar.Friday
+	case time.Saturday:
+		isDayActive = calendar.Saturday
+	case time.Sunday:
+		isDayActive = calendar.Sunday
+	}
+	return isDayActive == 1
+}
+
+func getActiveServicesOn(feed *gtfs.GTFS, moment time.Time) map[string]bool {
+	day := moment.Truncate(24 * time.Hour)
+	dayString := day.Format("20060102")
+	weekday := day.Weekday()
+
+	//get all service exceptions
+	negativeExceptions := make(map[string]bool)
+	var activeServices []string
+	for _, calendarDate := range feed.CalendarDates {
+		fmt.Printf("calendarDate: %v\n", calendarDate)
+		if dayString != calendarDate.Date {
+			continue
+		}
+		if calendarDate.ExceptionType == gtfs.ExceptionTypeAdded {
+			activeServices = append(activeServices, calendarDate.ServiceID)
+		} else {
+			negativeExceptions[calendarDate.ServiceID] = true
+		}
+	}
+
+	for _, calendar := range feed.Calendars {
+		fmt.Printf("calendar: %v\n", calendar)
+		startDate := dayFromFromGtfsString(calendar.Start)
+		endDate := dayFromFromGtfsString(calendar.End)
+		if day.Before(startDate) || day.After(endDate) {
+			continue
+		}
+		if isCalendarActiveOn(calendar, weekday) && !negativeExceptions[calendar.ServiceID] {
+			activeServices = append(activeServices, calendar.ServiceID)
+		}
+	}
+
+	activeServicesMap := make(map[string]bool)
+	for _, serviceId := range activeServices {
+		activeServicesMap[serviceId] = true
+	}
+	fmt.Printf("negativeExceptions: %v\n", negativeExceptions)
+	fmt.Printf("activeServices: %v\n", activeServices)
+	return activeServicesMap
+}
